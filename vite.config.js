7aaf60164c53fd79774774fc0d917e24f9d46ab0
sanaul ansari <ansarcode09@gmail.com>
2024-01-06 21:23:29 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/E-COMMERCE_WITH_REACT/",
+  base:"/ECOMMERCE_WITH_REACT/",
   plugins: [react()],
 })
